af0fbb17d1ed392a0708bc02dbecf87f93e4ed1d
Bryan Edward Geronimo <bryangero@gmail.com>
2016-06-06 23:55:03 +0800

    +shooter script

---
+++ Assets/_BubbleShooter/scripts/Shooter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public float rotationSpeed;
+	public GameObject bubbleGameObject;
+	public GameObject direction;
+
+	private void Update() {
+		if(Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Rotate(0f, 0f, rotationSpeed);
+		}
+		if(Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Rotate(0f, 0f, -rotationSpeed);
+		}
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			Bubble bubble = Instantiate(bubbleGameObject).GetComponent<Bubble>() as Bubble;
+			bubble.direction = direction.transform.position; 
+		}
+	}
+
+
+}
