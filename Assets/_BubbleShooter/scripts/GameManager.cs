4f3d556a1d470ebc2b4379dfa76da6eaa5fdf9c1
Bryan Edward Geronimo <bryangero@gmail.com>
2016-06-08 13:24:06 +0800

    +added game manager *updated shooter behaviour

---
+++ Assets/_BubbleShooter/scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class GameManager : MonoBehaviour {
+
+	public ArrayList bubbles = new ArrayList();
+
+	public delegate void PopBubbleDG(Color color);
+	public event PopBubbleDG PopBubbleEvent;
+
+
+	public void SubscribeToPopBubbleEvent(PopBubbleDG popBubbleDG) {
+		if (PopBubbleEvent == null || !PopBubbleEvent.GetInvocationList ().Contains (popBubbleDG)) {
+			PopBubbleEvent += popBubbleDG;
+		} else {
+			Debug.Log ("UNSUCCESSFUL BIND");
+		}
+	}
+
+	public void UnsubscribeToPopBubbleEvent(PopBubbleDG popBubbleDG) {
+		if (PopBubbleEvent.GetInvocationList().Contains(popBubbleDG)) {
+			PopBubbleEvent -= popBubbleDG;
+		} 
+	}
+
+	public void CallPopBubbleEvent(Color color) {
+		PopBubbleEvent(color);
+	}
+		
+}
--- Assets/_BubbleShooter/scripts/Shooter.cs
+++ Assets/_BubbleShooter/scripts/Shooter.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour 
 {
+
+	public delegate void ShootBubbleDG();
+	public event ShootBubbleDG ShootBubbleEvent;
+
+	private GameManager gameManager;
 	[SerializeField] private float rotationSpeed;
 	[SerializeField] private GameObject bubbleGameObject;
 	[SerializeField] private GameObject direction;
@@ -11,31 +16,34 @@ public class Shooter : MonoBehaviour
 
 	private void Start() 
 	{
+		gameManager = GameObject.FindObjectOfType(typeof(GameManager)) as GameManager;
 		ReloadBubble();	
 	}
 
 	public void ReloadBubble()
 	{
+		isBubbleShot = false;
 		bubble = Instantiate(bubbleGameObject).GetComponent<Bubble>() as Bubble;
 	}
-
+		
 	private void Update() 
 	{
-		float mouseAxisX = Input.GetAxis("Mouse X");
-		transform.Rotate(0f, 0f, -mouseAxisX * rotationSpeed);
+		if (isBubbleShot)
+			return;
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+		rot.x = 0;
+		rot.y = 0;
+		transform.rotation = rot;
+//		Debug.DrawLine(transform.position, transform.position + mousePosition);
 		if(Input.GetMouseButtonUp(0))
 		{
 			if (bubble != null) 
 			{
 				bubble.direction = direction.transform.position;
-				bubble.isMoving = true;
+				bubble.BubbleLandedEvent += ReloadBubble;
 				isBubbleShot = true;
 			} 
-		}
-		if (bubble.isMoving == false && isBubbleShot == true) 
-		{
-			isBubbleShot = false;
-			ReloadBubble();
 		}
 	}
 
